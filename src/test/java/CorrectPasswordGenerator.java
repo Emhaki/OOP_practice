770b44abdb0a2be1f7918678975b4cd55e10454e
emhaki <mhmh779@naver.com>
2023-06-19 14:18:41 +0900

    OOP_practice

--- src/test/java/CorrectPasswordGenerator.java
+++
@@ -1,8 +0,0 @@
-import org.example.PasswordGeneratePolicy;
-
-public class CorrectPasswordGenerator implements PasswordGeneratePolicy {
-    @Override
-    public String generatePassword() {
-        return "aaaabbbbcc";
-    }
-}
